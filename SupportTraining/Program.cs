aab53bc5a5f3f3f91e45af09f35e7c6e8ce5b2fc
Shane Delaney <shanebdelaneyii@gmail.com>
2020-03-18 20:04:50 -0230

    Finished SupportTraining Question

--- SupportTraining/Program.cs
+++ SupportTraining/Program.cs
@@ -6,8 +6,24 @@ namespace SupportTraining
 	{
 		static void Main(string[] args)
 		{
-
-			Console.WriteLine("Hello World!");
+			for (int i = 1; i <= 100; i++) {
+				if (i % 3 == 0 && i % 5 == 0)
+				{
+					Console.WriteLine("SupportTraining");
+				}
+				else if (i % 3 == 0)
+				{
+					Console.WriteLine("Support");
+				}
+				else if (i % 5 == 0)
+				{
+					Console.WriteLine("Training");
+				}
+				else
+				{
+					Console.WriteLine(i);
+				}
+			}
 		}
 	}
 }
